14dda32868c4c33c962f825cdab86292970c3b22
Jake Vo <JakeVo@ashn.com>
2018-05-08 15:20:03 -0700

    Add all files

--- app/controllers/todos/new.js
+++ app/controllers/todos/new.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    actions:{
+        addTodo: function(){
+            var date = this.get('date');
+            var title = this.get('title');
+            var body = this.get('body');
+            var todoObject = this.store.createRecord('todo', {
+                title: title,
+                date: new Date(date),
+                body: body
+            });
+
+            todoObject.save();
+
+            this.setProperties({
+                title: '',
+                body: ''
+            });
+        }
+    }
 });
